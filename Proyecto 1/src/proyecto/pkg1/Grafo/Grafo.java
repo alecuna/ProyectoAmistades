928ebc64e8e227d2b8935a0d43b248246658a746
Alexandra Lecuna <alecuna@correo.unimet.edu.ve>
2023-05-24 11:55:54 -0400

    Constructor y funciones clase grafo

---
+++ Proyecto 1/src/proyecto/pkg1/Grafo/Grafo.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg1.Grafo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alexandralecuna
+ */
+public class Grafo {
+    
+    private Lista<Integer>[] adj;
+    private int V; // numero de vertices
+    private int E; // numero de aristas
+    
+    public Grafo(int vertices) {
+        this.V = vertices;
+        this.E = 0;
+        this.adj = new Lista[vertices];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Lista<>();
+        }
+    }
+    
+    public void addArista(int a, int b) {
+        adj[a].insertLast(b);
+        adj[b].insertLast(a);
+        E++;
+    }
+    
+    public void printVerts() {
+        
+        String sVerts = "";
+        sVerts += V + " vertices, " + E + " edges " + "\n";
+        for (int v = 0; v < V; v++) {
+            sVerts += (v + ": ");
+            for (int i = 0; i < V; i++) {
+                sVerts += adj[v].getDato(i);
+            }
+        }
+        System.out.println(sVerts); 
+    }
+    
+//    public void print2(){
+//        
+//        String sVerts = "";
+//        sVerts += V + " vertices, " + E + " edges " + "\n";
+//        for (int v = 0; v < V; v++) {
+//            sVerts += (v + ": ");
+//            for (int w : adj[v]){
+//                sVerts += w + " ";
+//            }
+//        }
+//    }
+    
+}
--- Proyecto 1/src/proyecto/pkg1/Main.java
+++ Proyecto 1/src/proyecto/pkg1/Main.java
@@ -4,6 +4,8 @@
  */
 package proyecto.pkg1;
 
+import proyecto.pkg1.Grafo.Grafo;
+
 /**
  *
  * @author Anabella Jaua
@@ -14,7 +16,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Grafo m = new Grafo(4);
+        m.addArista(0, 1);
+        m.addArista(1, 2);
+        m.addArista(2, 3);
+        m.addArista(3, 0);
+        
+        m.printVerts();
+        
     }
     
 }
