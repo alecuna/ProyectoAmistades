647514fdad5222975438ea913ec73dd78b13d83f
María <mcuervo@correo.unimet.edu.ve>
2023-05-24 18:44:26 -0400

    Nueva version txt

--- Proyecto 1/${test.src.dir}/Lecturatxt/main.java
+++ Proyecto 1/${test.src.dir}/Lecturatxt/main.java
@@ -1,27 +1,27 @@
+
+import txt.AbrirGuardarTxt;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package txt;
 
 /**
  *
  * @author Maria Daniela
  */
-public class Txt {
+public class main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AbrirGuardarTxt man = new AbrirGuardarTxt();
-        man.setSize(700, 500);
-        man.setVisible(true);
-        man.setLocationRelativeTo(null);
-        man.setResizable(false);
+        AbrirGuardarTxt txt = new AbrirGuardarTxt();
+        txt.setSize(700, 500);
+        txt.setVisible(true);
+        txt.setLocationRelativeTo(null);
+        txt.setResizable(false);
 
-          
-          
     }
     
 }
