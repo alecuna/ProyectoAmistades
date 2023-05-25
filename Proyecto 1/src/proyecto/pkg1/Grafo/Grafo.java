5e760f93ccddf4173cd0926305c20ad57b5cce1d
Alexandra Lecuna <alecuna@correo.unimet.edu.ve>
2023-05-24 23:19:39 -0400

    Nuevas clases Vertex y User

--- Proyecto 1/src/proyecto/pkg1/Grafo/Grafo.java
+++ Proyecto 1/src/proyecto/pkg1/Grafo/Grafo.java
@@ -12,38 +12,52 @@ import javax.swing.JOptionPane;
  */
 public class Grafo {
 
-    private Lista<Integer>[] adj;
+    private Lista<Vertex>[] arrayAdj;
     private int V; // numero de vertices
-    private int E; // numero de aristas
+    private int A; // numero de aristas
 
     public Grafo(int vertices) {
         this.V = vertices;
-        this.E = 0;
-        this.adj = new Lista[vertices];
+        this.A = 0;
+        this.arrayAdj = new Lista[vertices];
         for (int i = 0; i < V; i++) {
-            adj[i] = new Lista<>();
+            arrayAdj[i] = new Lista<Vertex>();
         }
     }
 
-    public void addArista(int a, int b) {
-        adj[a].insertLast(b);
-        adj[b].insertLast(a);
-        E++;
+    public void addArista(Vertex a, Vertex b) {
+        arrayAdj[a].insertLast(b);
+        arrayAdj[b].insertLast(a);
+        A++;
     }
 
     public void printVerts() {
 
         String sVerts = "";
-        sVerts += V + " vertices, " + E + " edges " + "\n";
-        for (int v = 0; v < V; v++) {
-            Nodo pointer = adj[v].getHead();
-            sVerts += (v + ": ");
-            for (int i = 0; i < adj[v].getSize(); i++) {
-                sVerts += adj[v].getDato(i) + " ";
+        sVerts += V + " vertices, " + A + " aristas " + "\n";
+        for (int i = 0; i < V; i++) {
+            Nodo pointer = arrayAdj[i].getHead();
+            sVerts += (i + " -> ");
+            for (int j = 0; j < arrayAdj[i].getSize(); j++) {
+                sVerts += arrayAdj[i].getDato(j).getUser().getUsername() + " ";
             }
             sVerts += " \n";
         }
         System.out.println(sVerts);
     }
+    
+    public int findVert(User currentUser) {
+        Vertex vert = new Vertex(currentUser);
+        boolean found = false;
+        int i = 0;
+        
+        for (i = 0 ; (i < V) && !found ; i++) {
+            
+            found = arrayAdj[i].equals(vert);
+            if (!found) i++;     
+        }
+        return (i < V) ? i : -1;
+    }
+    
 
 }
---
+++ Proyecto 1/src/proyecto/pkg1/Grafo/User.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg1.Grafo;
+
+/**
+ *
+ * @author alexandralecuna
+ */
+public class User {
+    
+    private String username;
+    private int userID;
+
+    public User(String username, int userID) {
+        this.username = username;
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    
+    
+}
